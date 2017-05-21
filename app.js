2bef78c24433db9dbb1c0d5eda43a7449402d2e7
changxianglin <281532612@qq.com>
2017-05-22 00:44:23 +0800

    read & wrrite

--- app.js
+++ app.js
@@ -18,3 +18,34 @@ class App {
 }
 
 module.exports =  App
+
+// fs.readFile('./hello.txt', 'utf8', function(err, data){
+//     if(err){
+//         console.error(err)
+//     } else {
+//         console.log(data)
+//     }
+// })
+
+// fs.writeFile('./hello.txt', '这里是一个神奇的地方.',{
+//     flag: 'w'
+// }, function (err) {
+//     console.log(err)
+// })
+
+// const rs = fs.createReadStream('./hello.txt')
+// rs.on('data', function(chunk){
+//     console.log(chunk.toString())
+// })
+// const rs = fs.createReadStream('./15.jpg')
+// let count = 1;
+// var  result
+// rs.on('data', function(chunk){
+//     console.log(chunk)
+//     let counts = count++
+//     // console.log(counts)
+//     result += chunk
+// })
+// rs.on('end', function(){
+//     console.log(result)
+// })
