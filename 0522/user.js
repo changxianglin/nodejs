4ea65b5a51e61ca09d9857ef07d77058f8376017
changxianglin <281532612@qq.com>
2017-05-27 01:23:56 +0800

    new idea

--- 0522/user.js
+++
@@ -1,9 +0,0 @@
-function User(id, name, age) {
-    this.id = id
-    this.name = name
-    this.age = age
-    this.enter = function(){
-        console.log(this.name + 'inter lar')
-    }
-}
-module.exports = User
--- httpserver.md
+++
@@ -1,2 +0,0 @@
-+ 全局安装 npm install -g http-server
-+ 安装后可以在任意一个目录下直接运行命令行即可
