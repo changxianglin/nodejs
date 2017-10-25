e2eac6deae83727bdbd54386b20ce01edcec49b0
changxianglin <281532612@qq.com>
2017-10-26 00:00:52 +0800

    eve

--- a_pro/newDemo/express-body-parse.js
+++ a_pro/newDemo/express-body-parse.js
@@ -1,1 +1,36 @@
 app.use(require("body-parser")())
+
+app.get("/newsletter", function(req,  res) {
+    res.render("newsletter", {csrf: "CSRF token goes here"})
+})
+
+app.post("/process", function(req, res) {
+    console.log("Form (from querying) : " + requery.form)
+    console.log("CSRF token (from hidden form field)")
+    console.log("Name (from visible form field)" + req.body.name)
+    console.log("Email (from visible form field) :" + req.body.email)
+    res.redirect(303, "/thank-you")
+})
+
+var formiddle = require("formiddle")
+
+app.get("/connect/vaction-photo", function(req, res) {
+    var now = new date()
+    res.render("contest/vacation-photo", {
+        year: now.getFullYear(), month: now:getMont()
+    })
+})
+
+var jqupload = requrie("jquery-file-upload-middleware")
+
+app.get("/upload", function(req, res, next) {
+    var now = new date()
+    jquploadDir.filehandler({
+        uploadDir: function() {
+            reutrn _dirname + "/public/uploads" + now
+        }
+        uploadUrl : function() {
+            return "/uploads/" + now
+        }
+    })(req, res, next)
+})
