da94acc1d62be6c903da963004f33284ebd9baa1
changxianglin <281532612@qq.com>
2017-12-14 00:46:32 +0800

    net can model server & borwser

--- net/client.js
+++ net/client.js
@@ -3,12 +3,14 @@ const log = console.log.bind(console)
 
 const client = new net.Socket()
 
-const port = 80
-const host = '223.252.199.66'
+const port = 2000
+const host = '127.0.0.1'
+// const host = '223.252.199.66'
 client.connect(port, host, () => {
     log('connect to:', host, port)
 
-    const request = 'GET / HTTP/1.1\r\nHost: music.163.com\r\n\r\n'
+    // const request = 'GET / HTTP/1.1\r\nHost: music.163.com\r\n\r\n'
+    const request = 'client to link.'
     client.write(request)
 })
 
