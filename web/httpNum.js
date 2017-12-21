01f897ed7235420889487fc89e647d1026278594
zhang xianglin <zhangxianghuanlin@163.com>
2017-12-21 23:17:38 +0800

    database

--- web/httpNum.js
+++ web/httpNum.js
@@ -4,6 +4,7 @@ var counter = 0
 
 const server = http.createServer((req, res) => {
     counter++
+    // 可以接入 数据库
     res.write('I have been accessed ' + counter + ' times.')
     res.end()
 }).listen(8888)
