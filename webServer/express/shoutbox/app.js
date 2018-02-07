e62ee10f3e5eef0748129c2652c47d73e04c2514
changxianglin <281532612@qq.com>
2018-02-07 16:57:12 +0800

    node&web

--- webServer/express/shoutbox/app.js
+++ webServer/express/shoutbox/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var entries = require('./routes/entries')
 var index = require('./routes/index');
 var users = require('./routes/users');
 var register = require('./routes/register')
@@ -26,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', entries);
 app.use('/users', users);
 app.use('/register', register)
 app.use('/login', login)
