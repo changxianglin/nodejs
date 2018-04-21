f498a4af44add1fcc6a0a18d73f1107f40b6900e
changxianglin <281532612@qq.com>
2018-04-21 15:25:22 +0800

    create client to request server

--- webServer/review/second/NodeClient.js
+++ webServer/review/second/NodeClient.js
@@ -1,1 +1,26 @@
 const net = require('net')
+
+
+const host = 'blog.163.com'
+const port = 80
+
+
+const client = new net.Socket()
+
+client.connect({port, host}, () => {
+    console.log('now connect to ', host, port)
+
+    const request = 'GET / HTTP/1.1\r\nHost: blog.163.com\r\n\r\n'
+
+    client.write(request)
+})
+
+client.on('data', (data) => {
+    console.log('respone data', data.toString())
+
+    client.destroy()
+})
+
+client.on('close', () => {
+    console.log('connect closed')
+})
