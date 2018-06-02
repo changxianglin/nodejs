68867409abe22e93e021455bdff3b364db43bb3e
changxianglin <281532612@qq.com>
2018-06-03 02:31:35 +0800

    add database&get&post

--- baseweb/app.js
+++ baseweb/app.js
@@ -1,9 +1,22 @@
 var express = require('express')
-
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
 mongoose.connect('mongodb://localhost/test');
 
-const Cat = mongoose.model('Cat', { name: String });
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const Cat = mongoose.model('Cat', {
+    name: String,
+    age: String,
+    gender: String,
+});
+
+const Dog = mongoose.model('Dog', {
+    first: String,
+    end: String,
+})
 
 var  app = express()
 
@@ -16,9 +29,31 @@ app.get('/request', function (req, res) {
     res.send('request success!')
 })
 
-app.post('/info', function (req, res) {
-    const kitty = new Cat({ name: 'Zildjian to form' });
-    kitty.save().then(() => console.log('meow'));
+app.get('/ajax', function (req, res) {
+    res.sendFile(__dirname + '/ajax.html')
+})
+
+app.get('/ajaxinfo', function (req, res) {
+    console.log(req.query)
+    const ali = new Dog({
+        first: req.query.name,
+        end: req.query.end,
+    })
+    ali.save().then(() => {
+        console.log('ok')
+    })
+    res.json(req.query)
+})
+
+
+app.post('/info', urlencodedParser, function (req, res) {
+    const kitty = new Cat({
+        name: req.body.name,
+        age: req.body.firstname,
+        gender: req.body.gender,
+    });
+    kitty.save().then(() => console.log('meow'))
+    console.log(req.body)
     res.send('post can to server!!!')
 })
 
