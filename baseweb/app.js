b46a5e08810a7e348dd870e85a0b5d9ec1a90618
changxianglin <281532612@qq.com>
2018-06-03 15:37:13 +0800

    operating mongodb

--- baseweb/app.js
+++ baseweb/app.js
@@ -20,19 +20,22 @@ const Dog = mongoose.model('Dog', {
 
 var  app = express()
 
-
+// 主页
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
+// 第一个请求
 app.get('/request', function (req, res) {
     res.send('request success!')
 })
 
+// ajax 请求
 app.get('/ajax', function (req, res) {
     res.sendFile(__dirname + '/ajax.html')
 })
 
+// ajax 保存数据
 app.get('/ajaxinfo', function (req, res) {
     console.log(req.query)
     const ali = new Dog({
@@ -45,7 +48,15 @@ app.get('/ajaxinfo', function (req, res) {
     res.json(req.query)
 })
 
+// 查询数据
+app.get('/allinfo', function(req, res) {
+    Dog.find({}, {'_id':0, '__v': 0},function (err, kittens) {
+        if (err) return console.error(err);
+        res.json(kittens)
+    })
+})
 
+// 保存数据 post
 app.post('/info', urlencodedParser, function (req, res) {
     const kitty = new Cat({
         name: req.body.name,
@@ -57,7 +68,7 @@ app.post('/info', urlencodedParser, function (req, res) {
     res.send('post can to server!!!')
 })
 
-
+// 启动入口
 app.listen(5000, function () {
     console.log('app running port 5000')
 })
