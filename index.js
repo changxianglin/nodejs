0c8da614887a6291c85e4664f146601a1fe31b0f
湘林 <zhangxianghuanlin@163.com>
2018-07-07 14:26:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// ajax xhr === http  ====> 服务器 =====> 服务
-//  node.js
-const http = require('http')
-const PORT = 7000
-const App = require('./app.js')
-const server = new App()
-http.createServer(server.initServer()).listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`)
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('express pages')
 })
+
+module.exports = router
