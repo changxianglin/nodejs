8f5bd7cbbefcf513ed700a4f69e03eb7771c64a3
changxianglin <281532612@qq.com>
2019-04-04 00:18:05 +0800

    use mongodb crud

--- mongo-proj/app.js
+++ mongo-proj/app.js
@@ -21,7 +21,45 @@ app.post('/person', async (req, res, next) => {
     var person = new PersonModel(req.body)
     var result = await person.save()
     res.send(result)
-  } catch(err) {
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.get('/people', async (req, res, next) => {
+  try {
+    var result = await PersonModel.find().exec()
+    res.send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.get('/person/:id', async (req, res, next) => {
+  try {
+    var person = await PersonModel.findById(req.params.id).exec()
+    res.send(person)
+  } catch (err) {
+    res.send(500).send(err)
+  }
+})
+
+app.put('/person/:id', async (req, res) => {
+  try {
+    var person = await PersonModel.findById(req.params.id).exec()
+    person.set(req.body)
+    var result = await person.save()
+    res.send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.delete('/person/:id', async (req, res, next) => {
+  try {
+    var result = await PersonModel.deleteOne({ _id: req.params.id}).exec()
+    res.send(result)
+  } catch (err) {
     res.status(500).send(err)
   }
 })
