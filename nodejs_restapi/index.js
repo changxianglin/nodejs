459d201a8b26bde1ff6efb5efc40c37591e98faf
changxianglin <281532612@qq.com>
2019-04-03 23:12:28 +0800

    basic request and response

--- nodejs_restapi/index.js
+++ nodejs_restapi/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+app.use(morgan('short'))
 
 app.get('/', (req, res) => {
   console.log('Response to root route')
   res.send('Hello from ROOOT')
+})
+
+
+app.get('/users', (req, res) => {
+  var user1 = { firstName: 'Stephen', lastName: 'Curry' }
+  const user2 = { firstName: 'Kevin', lastName: 'Durant'}
+  res.json([user1, user2])
 })
 
 app.listen(3000, () => {
