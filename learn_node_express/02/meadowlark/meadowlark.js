ce75dea3ebbd6bcfd3fff9aed49a6394402a90f6
changxianglin <281532612@qq.com>
2019-04-08 16:03:58 +0800

    change data and show views

--- learn_node_express/02/meadowlark/meadowlark.js
+++ learn_node_express/02/meadowlark/meadowlark.js
@@ -11,12 +11,21 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(express.static(__dirname + '/public'))
 
+var fortunes = [
+  'Conquer your fears or they will conquer you.',
+  'Rivers need springs.',
+  'Do not fear waht you don\'t know',
+  'You will have a pleasan surprise.', 
+  'Whenever possible, keep it simple'
+]
+
 app.get('/', (req, res) => {
   res.render('home')
 })
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  res.render('about', { fortune: randomFortune })
 })
 
 app.use((req, res, next) => {
