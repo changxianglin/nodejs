93aba258efd08977dc51db2e0478314955d3cf95
changxianglin <281532612@qq.com>
2019-04-08 15:55:22 +0800

    crete public resource

--- learn_node_express/02/meadowlark/meadowlark.js
+++ learn_node_express/02/meadowlark/meadowlark.js
@@ -9,6 +9,8 @@ app.set('view engine', 'handlebars')
 
 app.set('port', process.env.PORT || 3000)
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.render('home')
 })
