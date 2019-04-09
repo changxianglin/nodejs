7bc400db181c24e4c1b1013d17b5620d0e77bb31
changxianglin <281532612@qq.com>
2019-04-09 23:57:03 +0800

    response form submit

--- learn_node_express/06/responseHeader.js
+++ learn_node_express/06/responseHeader.js
@@ -44,5 +44,26 @@ app.use((req, res) => {
   res.render('not-found')
 })
 
+app.post('/process-contact', (req, res) => {
+  console.log('Received contact from ' + req.body.name + '<' + req.body.email + '>')
+  // save to database
+  res.redirect(303, '/thank-yout')
+})
+
+app.post('/process-contact', (req, res) => {
+  console.log('Received contact from ' + req.body.name + ' <' + req.body.email + '>')
+  try {
+    //  save to database
+
+    return res.xhr ?
+      res.render({ success: true }) :
+      res.redirect(303, '/thank-you')
+  } catch(ex) {
+    return res.xhr ?
+      res.json({ error: 'Database error'}) :
+      res.redirect(303, '/database-error')
+  }
+})
+
 app.listen(3000)
 
