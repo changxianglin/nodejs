37263560efea932408e485ec638dfb33612ec701
changxianglin <281532612@qq.com>
2019-04-10 00:13:10 +0800

    a api have crud

--- learn_node_express/06/responseHeader.js
+++ learn_node_express/06/responseHeader.js
@@ -74,5 +74,57 @@ app.get('/api/tours', (req, res) => {
   res.json(tours)
 })
 
+app.get('/api/tours', (req, res) => {
+  var toursXml = '<?xml version="1.0"?><tours>' +
+    products.map((p) => {
+      return '<tour price="' + p.price + 
+        '" id="' + p.id + '">' + p.name + '</tour>'
+    }).join('') + '</tours>'
+  var toursText = tours.map((p) => {
+      return p.id + ': ' + p.name + ' (' + p.price + ')'
+    }).join('\n')
+  res.format({
+    'application/json': function() {
+      res.json(tours)
+    },
+    'application/xml': function() {
+      res.type('application/xml')
+      res.send(toursXml)
+    },
+    'text/xml': function() {
+      res.type('text/xml')
+      res.send(toursXml)
+    },
+    'text/plain': function() {
+      res.type('text/plian')
+      res.send(toursXml)
+    }
+  })
+})
+
+app.put('/api/tour/:id', (req, res) => {
+  var p = tours.some(function(p) { return p.id === req.params.id })
+  if(p) {
+    if( req.query.name ) p.name = req.query.name
+    if( req.query.price ) p.price = req.query.price
+    res.json({ success: true })
+  } else {
+    res.json({ error: 'No such tour exists.' })
+  }
+})
+
+app.delete('/api/tour/:id', (req, res) => {
+  var i
+  for (var i = tours.length - 1; i >= 0; i--) {
+    if(tours[i].id === req.params.id) break
+    if(i >= 0) {
+      tours.splice(i, 1)
+      res.json({ success: true })
+    } else {
+      res.json({ error: 'No such tour exists.'})
+    }
+  }
+})
+
 app.listen(3000)
 
