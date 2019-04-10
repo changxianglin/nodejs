1cefbc6daa1458a538308d56cf807a8385d60466
changxianglin <281532612@qq.com>
2019-04-10 23:54:57 +0800

    middleware

--- learn_node_express/02/meadowlark/meadowlark.js
+++ learn_node_express/02/meadowlark/meadowlark.js
@@ -48,6 +48,20 @@ app.get('/cookie', (req, res) => {
 })
 
 app.use((req, res, next) => {
+  console.log('processing request for "' + req.url + '"....')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('terminating request')
+  res.send('thanks for playing!')
+})
+
+app.use((req, res, next) => {
+  console.log('whoops, i\'ll never get called!')
+})
+
+app.use((req, res, next) => {
   res.status(404)
   res.render('404')
 })
