770ac3bac128d833980c55d9dfc7dc425326e9a5
changxianglin <281532612@qq.com>
2019-04-10 23:34:18 +0800

    set cookie and session

--- learn_node_express/02/meadowlark/meadowlark.js
+++ learn_node_express/02/meadowlark/meadowlark.js
@@ -2,15 +2,19 @@ var express = require('express')
 var exphbs = require('express-handlebars')
 var bodyParser = require('body-parser')
 var fromidable = require('formidable')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 var fortune = require('./lib/fortune')
 
 var app = express()
 
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.set('port', process.env.PORT || 3000)
 
@@ -34,6 +38,13 @@ app.post('/process', (req, res) => {
   console.log('Name (from visible from field): ' + req.body.name)
   console.log('Email (from visible from field): ' + req.body.email)
   res.redirect(303, '/thank-you')
+})
+
+app.get('/cookie', (req, res) => {
+  console.log('cookie: ', req.cookies)
+  console.log('Singed Cookies: ' + req.signedCookies)
+  res.cookie('singed_monster', 'nom nom', { isnged: true })
+  res.end()
 })
 
 app.use((req, res, next) => {
