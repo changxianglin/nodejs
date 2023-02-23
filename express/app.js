2f4614ebab47ea138e3d484e7d8b7435bbe5c61e
changxianglin <281532612@qq.com>
2023-02-23 21:57:27 +0800

    add methods get and page

--- express/app.js
+++ express/app.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 const port = 3000
+
+app.get('/index.html', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'index.html')
+  const contents = fs.readFileSync(filePath, 'utf-8')
+  res.send(contents)
+
+  const query = req.query
+  console.log(query)
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
