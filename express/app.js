f2e73d1887a86841e033f57cd4be1022fcbeb319
changxianglin <281532612@qq.com>
2023-02-24 21:17:59 +0800

    method post and redirect

--- express/app.js
+++ express/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.json())
 
 app.get('/index.html', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'index.html')
@@ -11,6 +15,26 @@ app.get('/index.html', (req, res) => {
 
   const query = req.query
   console.log(query)
+})
+
+app.get('/register', function(req, res) {
+  const filePath = path.join(__dirname, 'views', 'register.html')
+  const contents = fs.readFileSync(filePath, 'utf-8')
+  res.send(contents)
+})
+
+app.post('/register', function(req, res) {
+  let body = req.body 
+  const { username, email, password } = body
+  console.log(username, email, password)
+  // res.send('post 提交成功')
+  res.redirect('/login.html')
+})
+
+app.get('/login.html', function(req, res) {
+  const filePath = path.join(__dirname, 'views', 'login.html')
+  const contents = fs.readFileSync(filePath, 'utf-8')
+  res.send(contents)
 })
 
 app.get('/', (req, res) => {
