1d6a21d3a6ffee96ff2f401a3c6d11dd51e50fc3
changxianglin <281532612@qq.com>
2023-03-07 00:11:00 +0800

    use router

--- express/app.js
+++ express/app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const routers = require('./routers/passport')
+
+app.use(routers)
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extends: false}))
 app.use(bodyParser.json())
@@ -17,63 +21,63 @@ app.set('views options', {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'html')
 
-app.get('/render', (req, res) => {
-  let dataList = {
-    name: 'Jack',
-    age: 17,
-    job: 'fe',
-    childrens: ['Tom', 'John', 'Tony']
-  }
-  res.render('index.html', dataList)
-})
+// app.get('/render', (req, res) => {
+//   let dataList = {
+//     name: 'Jack',
+//     age: 17,
+//     job: 'fe',
+//     childrens: ['Tom', 'John', 'Tony']
+//   }
+//   res.render('index.html', dataList)
+// })
 
-app.get('/index.html', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'index.html')
-  const contents = fs.readFileSync(filePath, 'utf-8')
-  res.send(contents)
+// app.get('/index.html', (req, res) => {
+//   const filePath = path.join(__dirname, 'views', 'index.html')
+//   const contents = fs.readFileSync(filePath, 'utf-8')
+//   res.send(contents)
 
-  const query = req.query
-  console.log(query)
-})
+//   const query = req.query
+//   console.log(query)
+// })
 
-// app.get('/register', function(req, res) {
-//   const filePath = path.join(__dirname, 'views', 'register.html')
+// // app.get('/register', function(req, res) {
+// //   const filePath = path.join(__dirname, 'views', 'register.html')
+// //   const contents = fs.readFileSync(filePath, 'utf-8')
+// //   res.send(contents)
+// // })
+
+// // app.post('/register', function(req, res) {
+// //   let body = req.body 
+// //   const { username, email, password } = body
+// //   console.log(username, email, password)
+// //   // res.send('post 提交成功')
+// //   res.redirect('/login.html')
+// // })
+
+// app.all('/register', function(req, res) {
+//   const method = req.method
+//   if(method == 'GET') {
+//       const filePath = path.join(__dirname, 'views', 'register.html')
+//       const contents = fs.readFileSync(filePath, 'utf-8')
+//       res.send(contents)
+//   } else if(method == 'POST') {
+//       let body = req.body 
+//       const { username, email, password } = body
+//       console.log(username, email, password)
+//       // res.send('post 提交成功')
+//       res.redirect('/login.html')
+//   }
+// })
+
+// app.get('/login.html', function(req, res) {
+//   const filePath = path.join(__dirname, 'views', 'login.html')
 //   const contents = fs.readFileSync(filePath, 'utf-8')
 //   res.send(contents)
 // })
 
-// app.post('/register', function(req, res) {
-//   let body = req.body 
-//   const { username, email, password } = body
-//   console.log(username, email, password)
-//   // res.send('post 提交成功')
-//   res.redirect('/login.html')
+// app.get('/', (req, res) => {
+//   res.send('hello')
 // })
-
-app.all('/register', function(req, res) {
-  const method = req.method
-  if(method == 'GET') {
-      const filePath = path.join(__dirname, 'views', 'register.html')
-      const contents = fs.readFileSync(filePath, 'utf-8')
-      res.send(contents)
-  } else if(method == 'POST') {
-      let body = req.body 
-      const { username, email, password } = body
-      console.log(username, email, password)
-      // res.send('post 提交成功')
-      res.redirect('/login.html')
-  }
-})
-
-app.get('/login.html', function(req, res) {
-  const filePath = path.join(__dirname, 'views', 'login.html')
-  const contents = fs.readFileSync(filePath, 'utf-8')
-  res.send(contents)
-})
-
-app.get('/', (req, res) => {
-  res.send('hello')
-})
 
 app.listen(port, () => {
   console.log('express server running')
---
+++ express/routers/passport.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express.Router()
+
+app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.json())
+
+app.get('/render', (req, res) => {
+  let dataList = {
+    name: 'Jack',
+    age: 17,
+    job: 'fe',
+    childrens: ['Tom', 'John', 'Tony']
+  }
+  res.render('index.html', dataList)
+})
+
+app.get('/index.html', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'index.html')
+  const contents = fs.readFileSync(filePath, 'utf-8')
+  res.send(contents)
+
+  const query = req.query
+  console.log(query)
+})
+
+// app.get('/register', function(req, res) {
+//   const filePath = path.join(__dirname, 'views', 'register.html')
+//   const contents = fs.readFileSync(filePath, 'utf-8')
+//   res.send(contents)
+// })
+
+// app.post('/register', function(req, res) {
+//   let body = req.body 
+//   const { username, email, password } = body
+//   console.log(username, email, password)
+//   // res.send('post 提交成功')
+//   res.redirect('/login.html')
+// })
+
+app.all('/register', function(req, res) {
+  const method = req.method
+  if(method == 'GET') {
+      const filePath = path.join(__dirname, 'views', 'register.html')
+      const contents = fs.readFileSync(filePath, 'utf-8')
+      res.send(contents)
+  } else if(method == 'POST') {
+      let body = req.body 
+      const { username, email, password } = body
+      console.log(username, email, password)
+      // res.send('post 提交成功')
+      res.redirect('/login.html')
+  }
+})
+
+app.get('/login.html', function(req, res) {
+  const filePath = path.join(__dirname, 'views', 'login.html')
+  const contents = fs.readFileSync(filePath, 'utf-8')
+  res.send(contents)
+})
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+const routers = app
+
+module.exports = routers
